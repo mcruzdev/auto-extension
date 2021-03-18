8efa2db889cdba2e151d88ca3ebe66b06acf4317
Oiceotse Moura <ceotmoura@hotmail.com>
2021-03-18 00:24:36 -0300

    ajustes de layout de exibiçãio

--- modules/background/background.js
+++ modules/background/background.js
@@ -13,37 +13,61 @@ const showNotification = (params) => {
 
 const api = {
   submitCpf: async (cpf) => {
-    return await fetch("http://ec2-18-228-118-253.sa-east-1.compute.amazonaws.com:8080/api/v1/pe", {
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "*/*",
-      },
-      method: "POST",
-      body: JSON.stringify({cpf}),
-    });
+    return await fetch(
+      "http://ec2-18-228-118-253.sa-east-1.compute.amazonaws.com:8080/api/v1/pe",
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "*/*",
+        },
+        method: "POST",
+        body: JSON.stringify({ cpf }),
+      }
+    );
   },
 };
 
 const postCpf = (cpf) => {
-  api.submitCpf(cpf).then(async (res) => {
-    if (res.ok) {
-      const notificationParams = {
-        title: "AE Digitals",
-        message: "Seu cadastro foi realizado com sucesso",
-      };
-      alert(notificationParams.message);
-      showNotification(notificationParams);
-    } else {
-      const notificationParams = {
-        title: "AE Digitals",
-        message: "Não foi possível realizar seu cadastro, tente novamente :(",
-      };
-      showNotification(notificationParams);
-      alert(notificationParams.message);
-    }
-  });
+  try {
+    api
+      .submitCpf(cpf)
+      .then(async (res) => {
+        if (res.ok) {
+          const data = await res.json();
+          const notificationParams = {
+            title: "AE Digitals",
+            message: `${data.alunoName}, seu cadastro foi realizado com sucesso`,
+          };
+          showNotification(notificationParams);
+          alert(notificationParams.message);
+        } else {
+          const data = await res.json();
+          const notificationParams = {
+            title: "AE Digitals",
+            message: `${data.alunoName}, não foi possível realizar seu cadastro, tente novamente :(`,
+          };
+          showNotification(notificationParams);
+          alert(notificationParams.message);
+        }
+      })
+      .catch(() => {
+        const notificationParams = {
+          title: "AE Digitals",
+          message: "Não foi possível realizar seu cadastro, tente novamente :(",
+        };
+        showNotification(notificationParams);
+        alert(notificationParams.message);
+      });
+  } catch (err) {
+    const notificationParams = {
+      title: "AE Digitals",
+      message: "Não foi possível realizar seu cadastro, tente novamente :(",
+    };
+    showNotification(notificationParams);
+    alert(notificationParams.message);
+  }
 };
 
 chrome.runtime.onMessage.addListener((cpf) => {
-  postCpf(cpf)
+  postCpf(cpf);
 });
